0be893156b50eee9271afea86731057f89bfee73
Isolator <180848105+CrowsInTrenchCoat@users.noreply.github.com>
2024-09-20 00:32:59 -0700

    First pass at converting to astro.

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+/**
+ * Astro Configuration
+ *
+ * @see https://astro.build/config
+ */
+export default defineConfig({
+  site: 'https://crowsintrenchcoat.github.io',
+  base: '/isolations',
+})
