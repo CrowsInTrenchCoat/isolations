977157143bc6b1d05758991559d5544f8a5df3d9
Crows <180848105+CrowsInTrenchCoat@users.noreply.github.com>
2024-09-24 05:19:16 -0700

    Mucking about with the pie chart demo.

--- src/app/(demos)/pie-chart/demo.tsx
+++ src/app/(demos)/pie-chart/demo.tsx
@@ -1,25 +1,41 @@
 'use client'
 
+import { useState } from 'react'
 import { PieChart, Slice } from '@/svg/pie-chart'
+import { readWholeNumber } from '@/lib/read/whole-number'
 
 interface DemoProps {
   diameter: number
 }
 
+interface SliceData {
+  value : number,
+  fill : string,
+}
+
 export function Demo (props : DemoProps) {
+  const diameter = readWholeNumber(props.diameter) || 300
+  const intialSlices = [
+    { value: 1, fill: 'black' },
+    { value: 1, fill: 'hotpink' },
+    { value: 1, fill: 'black' },
+    { value: 1, fill: 'hotpink' },
+    { value: 1, fill: 'black' },
+    { value: 1, fill: 'hotpink' },
+  ]
+  const [ slices, setSlices ] = useState<SliceData[]>(intialSlices)
   return (
     <>
       <div className="figure">
         <div className="figure-subject">
-          <PieChart diameter={300} >
-            <Slice value={1} fill="black" />
-            <Slice value={1} fill="hotpink" />
-            <Slice value={1} fill="black" />
-            <Slice value={1} fill="hotpink" />
+          <PieChart diameter={diameter}>
+            {slices.map((slice, index) => {
+              return <Slice title="Gremlins" key={index} {...slice} />
+            })}
           </PieChart>
         </div>
         <div className="figure-actions">
-
+          <button>Add slice</button>
         </div>
       </div>
     </>
