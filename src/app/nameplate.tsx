5fc7ffd049e3936bad70609b85caa0f78d9e97bd
Crows <180848105+CrowsInTrenchCoat@users.noreply.github.com>
2024-09-26 10:56:56 -0700

    code formatting

--- src/app/nameplate.tsx
+++ src/app/nameplate.tsx
@@ -3,7 +3,9 @@ import { bangers } from '@/fonts'
 export function Nameplate () {
   return (
     <header id="nameplate">
-      <span id="nameplate-name" className={bangers.className}><abbr title="JavaScript">JS</abbr> Laboratory</span>
+      <span id="nameplate-name" className={bangers.className}>
+        <abbr title="JavaScript">JS</abbr> Laboratory
+      </span>
     </header>
   )
 }
